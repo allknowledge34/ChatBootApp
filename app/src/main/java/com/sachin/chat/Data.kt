d97bfe9e7db4f953ba01af74ce4b45c7a7d262cd
sachin kumar <allknowledge34@gmail.com>
2024-12-21 17:19:30 +0530

    this is first commit

---
+++ app/src/main/java/com/sachin/chat/Data.kt
@@ -0,0 +1,6 @@
+package com.sachin.chat
+
+object Data {
+
+    val apiKey = "AIzaSyDgwCHmNoB7XLO73GDQ-Ua9pSMOfjVvD50"
+}
---
+++ app/src/main/java/com/sachin/chat/ModelActivity.kt
@@ -0,0 +1,6 @@
+package com.sachin.chat
+
+data class ModelActivity(
+    val message : String,
+    val role : String,
+)
